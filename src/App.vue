f739dbe3a43cdf23db239c3371ce28c6a86f8606
Phil Ho <phil@philho.com>
2021-12-13 09:20:24 -0500

    Convert to Vite and add CSS styling.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
-<template>
-  <DiscountCalculator/>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import DiscountCalculator from '@/components/DiscountCalculator.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    DiscountCalculator
-  }
-});
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import DiscountCalculator from './components/DiscountCalculator.vue'
 </script>
 
+<template>
+  <DiscountCalculator />
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+body {
+    font-family: 'Open Sans', sans-serif;
+    margin: 0;
+    padding: 0;
+    color: rgb(39, 39, 39);
+    background-color: rgb(209, 231, 255);
+}
+
+h1, h2 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+  width: 80%;
+  margin: auto;
+  background-color: white;
+  padding: 1.5rem;
+  border-radius: 8px;
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.form-control,
+.form-control-overflow {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.form-control label,
+.form-control-overflow label {
+  flex-grow: 1;
+}
+
+.form-control input,
+.form-control-overflow input {
+  flex-grow: 2;
+  margin: 0.5rem;
+}
+
+button {
+  background-color: royalblue;
+  color: white;
+  border-radius: 5px;
+  border: 1px solid royalblue;
+  margin: 0 .5rem;
+  padding: 0.5rem;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: rgb(18, 70, 224);
 }
 </style>
--- src/components/DiscountCalculator.vue
+++ src/components/DiscountCalculator.vue
@@ -1,92 +1,156 @@
 <template>
-  <h1>Discount Calculator</h1>
+    <h1>Discount Calculator</h1>
 
-  <label>Price $</label>
-  <input type="number" v-model="price" />
+    <main>
+        <section>
+            <div class="form-control">
+                <label>Price $</label>
+                <input type="number" v-model="price" />
+            </div>
 
-  <label>Tax %</label>
-  <input type="number" v-model="tax" />
+            <div class="form-control">
+                <label>Tax %</label>
+                <input type="number" v-model="tax" />
+            </div>
 
-  <h2>Simple Discount</h2>
+            <h2>Simple Discount</h2>
 
-  <label>Discount %</label>
-  <input type="number" v-model="discount" />
+            <div class="form-control">
+                <label>Discount %</label>
+                <input type="number" v-model="discount" />
+            </div>
 
-  <p>Grand Total ${{ grandTotal }}</p>
+            <div id="buttons">
+                <button @click="applySimpleDiscount(30)">30%</button>
+                <button @click="applySimpleDiscount(40)">40%</button>
+                <button @click="applySimpleDiscount(50)">50%</button>
+                <button @click="applySimpleDiscount(60)">60%</button>
+            </div>
 
-  <h2>BOGO</h2>
+            <p>
+                <strong>Grand Total</strong>
+                : ${{ grandTotal }}
+            </p>
+        </section>
 
-  <label>% Off Second Item</label>
-  <input type="number" v-model="bogoDiscount" />
+        <section>
+            <h2>BOGO</h2>
 
-  <p>Grand Total for Two Items ${{ bogoGrandTotal }}</p>
-  <p>Price Per Item ${{bogoPricePerItem}}</p>
+            <div class="form-control-overflow">
+                <label>% Off 2nd Item</label>
+                <input type="number" v-model="bogoDiscount" />
+            </div>
+
+            <p>
+                <strong>Grand Total for Two Items</strong>
+                : ${{ bogoGrandTotal }}
+            </p>
+            <p>
+                <strong>Price Per Item</strong>
+                : ${{ bogoPricePerItem }}
+            </p>
+        </section>
+    </main>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed } from "vue";
 
 export default defineComponent({
-  setup() {
-    const price = ref<number>(0);
-    const discount = ref<number>(0);
-    const tax = ref<number>(7.5);
-    const bogoDiscount = ref<number>(0);
+    setup() {
+        const price = ref<number>(0);
+        const discount = ref<number>(0);
+        const tax = ref<number>(7.5);
+        const bogoDiscount = ref<number>(0);
 
-    const taxRate = computed((): number => {
-      return tax.value / 100;
-    });
-    const discountRate = computed((): number => {
-      if (discount.value <= 0) {
-        return 0;
-      }
-      return (100 - discount.value) / 100;
-    });
-    const bogoDiscountRate = computed((): number => {
-      if (bogoDiscount.value <= 0) {
-        return 0;
-      }
-      return (100 - bogoDiscount.value) / 100;
-    });
+        const taxRate = computed((): number => {
+            return tax.value / 100;
+        });
+        const discountRate = computed((): number => {
+            if (discount.value <= 0) {
+                return 0;
+            }
+            return (100 - discount.value) / 100;
+        });
+        const bogoDiscountRate = computed((): number => {
+            if (bogoDiscount.value <= 0) {
+                return 0;
+            }
+            return (100 - bogoDiscount.value) / 100;
+        });
 
-    const grandTotal = computed((): number => {
-      let discountedPrice =
-        discountRate.value > 0 ? price.value * discountRate.value : price.value;
-      let totalTax = discountedPrice * taxRate.value;
+        const grandTotal = computed((): number => {
+            let discountedPrice =
+                discountRate.value > 0 ? price.value * discountRate.value : price.value;
+            let totalTax = discountedPrice * taxRate.value;
 
-      return roundToTwo(+discountedPrice + totalTax);
-    });
+            return roundToTwo(+discountedPrice + totalTax);
+        });
 
-    const bogoGrandTotal = computed((): number => {
-      let discountedPrice = price.value * 2;
+        const bogoGrandTotal = computed((): number => {
+            let discountedPrice = price.value * 2;
 
-      if (bogoDiscountRate.value > 0) {
-        discountedPrice =
-          +(price.value * bogoDiscountRate.value) + +price.value;
-      }
+            if (bogoDiscountRate.value > 0) {
+                discountedPrice =
+                    +(price.value * bogoDiscountRate.value) + +price.value;
+            }
 
-      let totalTax = discountedPrice * taxRate.value;
+            let totalTax = discountedPrice * taxRate.value;
 
-      return roundToTwo(discountedPrice + totalTax);
-    });
+            return roundToTwo(discountedPrice + totalTax);
+        });
 
-    const bogoPricePerItem = computed((): number => {
-      return roundToTwo(bogoGrandTotal.value / 2);
-    })
+        const bogoPricePerItem = computed((): number => {
+            return roundToTwo(bogoGrandTotal.value / 2);
+        })
 
-    const roundToTwo: number = (num: number) => {
-      return +(Math.round(+(num + "e+2")) + "e-2");
-    };
+        const roundToTwo = (num: number) => {
+            return Math.round(num * 100) / 100;
+        }
 
-    return {
-      price,
-      discount,
-      tax,
-      grandTotal,
-      bogoGrandTotal,
-      bogoDiscount,
-      bogoPricePerItem
-    };
-  },
+        const applySimpleDiscount = (discountAmount: number) => {
+            discount.value = discountAmount;
+        }
+
+        return {
+            price,
+            discount,
+            tax,
+            grandTotal,
+            bogoGrandTotal,
+            bogoDiscount,
+            bogoPricePerItem,
+            applySimpleDiscount
+        };
+    },
 });
 </script>
+
+<style scoped>
+#buttons {
+    text-align: center;
+}
+
+@media (min-width: 48rem) {
+    main {
+        display: flex;
+        justify-content: space-between;
+        width: 70%;
+        gap: 5rem;
+    }
+
+    section {
+        flex-grow: 1;
+    }
+
+    .form-control label {
+        text-align: center;
+    }
+
+    .form-control-overflow label,
+    .form-control-overflow input {
+        display: block;
+        width: 100%;
+    }
+}
+</style>
